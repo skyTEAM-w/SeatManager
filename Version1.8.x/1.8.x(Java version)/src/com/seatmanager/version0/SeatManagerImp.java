bebbaa032fd9265654dd0031fd734e3c82232de3
WuChengpei_Sky <w2563070231@outlook.com>
2022-02-09 22:52:05 +0800

    Demo

--- Version1.8.x/1.8.x(Java version)/src/com/seatmanager/version0/SeatManagerImp.java
+++ Version1.8.x/1.8.x(Java version)/src/com/seatmanager/version0/SeatManagerImp.java
@@ -1,5 +1,27 @@
 package com.seatmanager.version0;
 
-public class SeatManagerImp {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class SeatManagerImp implements KeyListener {
+
+    public static void showUI() {
+
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
