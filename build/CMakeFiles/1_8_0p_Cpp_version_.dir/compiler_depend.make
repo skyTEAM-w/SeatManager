844779a35cc9cca388bd7ec93717911e18465eb3
WuChengpei_Sky <w2563070231@outlook.com>
2026-01-12 18:39:40 +0800

    chore: 清理build目录并更新.gitignore

--- build/CMakeFiles/1_8_0p_Cpp_version_.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for 1_8_0p_Cpp_version_.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/1_8_0p_Cpp_version_.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for 1_8_0p_Cpp_version_.
